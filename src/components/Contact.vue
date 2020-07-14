868108e4ac6cab23339d96b2ff96f034a706fc63
Adam Plocher <aplocher@live.com>
2020-07-13 22:34:33 -0700

    Added input fields

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,17 +1,59 @@
 <template>
   <div>
-    <div>
-      First name: {{ contact.firstName }}
-    </div>
-    <div>
-      Last name: {{ contact.lastName }}
-    </div>
-    <div>
-      Email: {{ contact.email }}
-    </div>
-    <div>
-      Phone: {{ contact.phone }}
-    </div>
+    <b-form
+      @submit="onSubmit"
+      @reset="onReset"
+    >
+      <b-row>
+        <b-col>
+          <b-form-group label="First name:">
+            <b-form-input
+              id="firstNameInput"
+              v-model="contact.firstName"
+            />
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <b-form-group label="Last name:">
+            <b-form-input
+              id="lastNameInput"
+              v-model="contact.lastName"
+            />
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <b-form-group label="Email:">
+            <b-form-input
+              id="emailInput"
+              v-model="contact.email"
+            />
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <b-form-group label="Phone:">
+            <b-form-input
+              id="phoneInput"
+              v-model="contact.phone"
+            />
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <b-form-group>
+            <b-form-checkbox
+              v-model="contact.isActive"
+              name="check-button"
+              switch
+            >
+              {{ contact.isActive ? "Active" : "Inactive" }}
+            </b-form-checkbox>
+          </b-form-group>
+        </b-col>
+      </b-row>
+    </b-form>
   </div>
 </template>
 <script>
@@ -24,7 +66,13 @@ export default {
       }
     }
   },
-  methods: {}
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.contact));
+    },
+    onReset() {}
+  }
 };
 </script>
 
